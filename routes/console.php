ccc491fb3cbf238b3cb0f44da1775c3d3afa0ebe
Dzaky-23 <dzaky2355@students.unnes.ac.id>
2026-06-08 14:09:01 +0700

    test

--- routes/console.php
+++ routes/console.php
@@ -9,9 +9,15 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Artisan::command('recap-top:build', function (RekapPenyakitTopBuilder $builder) {
+Artisan::command('recap-top:build {--reset}', function (RekapPenyakitTopBuilder $builder) {
+    if ($this->option('reset')) {
+        $this->info('Mengosongkan tabel rekapitulasi...');
+        Illuminate\Support\Facades\DB::table('rekap_penyakit_top')->truncate();
+        Illuminate\Support\Facades\DB::table('rekap_logs')->where('job_name', 'recap-top-build')->delete();
+        $this->info('Tabel rekapitulasi berhasil dikosongkan.');
+    }
     $builder->build();
     $this->info('Rekap penyakit top-N berhasil dibuat ulang.');
 })->purpose('Membangun ulang tabel rekap penyakit top-N.');
 
-Schedule::command('recap-top:build')->dailyAt('14:32')->withoutOverlapping();
+Schedule::command('recap-top:build')->dailyAt('09:52')->withoutOverlapping();
